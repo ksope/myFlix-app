cdbb6a75936395d2c85e202b04916547fa2c7bc9
ksope <105855785+ksope@users.noreply.github.com>
2023-06-27 12:39:51 +0100

    Summary
    Started a fresh repo and discarded 'myFlix-client" repo. This was due to not being able to get parcel to build the app on the previous repo

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import { createRoot } from "react-dom/client";
+import MainView from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
+
+//Import statement to indicate that you need to bundle `./index.scss`
+import "./index.scss";
+
+//Main component (will eventualy use all the others)
+const MyFlixApplication = () => {
+    return (
+        <Container className="text-bg-dark mx-auto" fluid>
+            <MainView />
+        </Container>
+    );
+};
+
+// Finds the root of your app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+//Tells React to render your app in the root DOM element
+root.render(<MyFlixApplication />);
